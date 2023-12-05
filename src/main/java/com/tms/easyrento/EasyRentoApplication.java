cb662e47ce198a15adda507752be67065781484f
shashi <shashipanta57@gmail.com>
2023-12-05 23:20:05 +0545

    init: created initial project setup

---
+++ src/main/java/com/tms/easyrento/EasyRentoApplication.java
@@ -0,0 +1,13 @@
+package com.tms.easyrento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyRentoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tms/easyrento/EasyRentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.easyrento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
