83223ebdc60445f3298a5a6500c04f61afdec440
shashi <shashipanta57@gmail.com>
2024-03-11 00:52:09 +0545

    feat(frontend): added cors configuration for react app

--- src/main/java/com/tms/easyrento/config/cors/CorsGlobalConfig.java
+++ src/main/java/com/tms/easyrento/config/cors/CorsGlobalConfig.java
@@ -23,7 +23,8 @@ public class CorsGlobalConfig implements WebMvcConfigurer{
             "http://127.0.0.1:5173",
             "http://localhost:5173",
             "http://localhost:5174",
-            "http://127.0.0.1:5174"
+            "http://127.0.0.1:5174",
+            "http://localhost:3000",
     };
 
     /**
