680f0a6e51fbb8a99b8c3be618f27a1f1dd4a649
barbosa <shashipanta57@gmail.com>
2025-08-01 22:09:18 +0545

    fix(web-socket): add allowed origin and remove sockJS fallback
    * explicit allowed origins are added
    * sockJS fallback is removed as it created problem with /ws/iframe

--- src/main/java/com/tms/easyrento/config/websocket/WebSocketConfig.java
+++ src/main/java/com/tms/easyrento/config/websocket/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.tms.easyrento.config.websocket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -7,16 +8,27 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final String[] allowedOrigins = {
+            "http://127.0.0.1:5173",
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
+            "http://localhost:3000",
+    };
+
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
     /*
-        Use of /app: all the messages will be routed to methods anotated
+        Use of /app: all the messages will be routed to methods annotated
         with @MessageMapping()
      */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic/private", "/topic/group");
         config.setApplicationDestinationPrefixes("/app");
     }
 
@@ -26,7 +38,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();      // fallback for browsers not supporting web socket
+                .addInterceptors(jwtHandshakeInterceptor)
+                .setAllowedOrigins(allowedOrigins);
+//                .withSockJS();      // fallback for browsers not supporting web socket
     }
 }
